231c08c348da73a2b4f0b95a80fe968d231ee877
David Qian <dqian96@gmail.com>
2018-06-09 00:52:46 -0700

    better solns for 1 and 2

--- 1_multiples_of_3_and_5.py
+++ 1_multiples_of_3_and_5.py
@@ -4,17 +4,42 @@
 # Simple single-pass to accumulate multiples of 3 and (multiples of 5 but not 3)
 
 
-n = 1000
+# n = 1000
 
-m3 = 3
-m5 = 5
-acc = 0
+# m3 = 3
+# m5 = 5
+# acc = 0
 
-while m3 < n:
-    acc += m3
-    m3 += 3
-    if m5 < n and m5 % 3 != 0:
-        acc += m5
-    m5 += 5
+# while m3 < n:
+#     acc += m3
+#     m3 += 3
+#     if m5 < n and m5 % 3 != 0:
+#         acc += m5
+#     m5 += 5
 
-print(acc)
+# print(acc)
+
+"""
+    There exists a closed form solution to this question.
+
+    Notice that for the sum of multiples of a given number x, you can factor out x to get a arithmetic series
+    from 1 to n. We know the formula for this series as n(n + 1)/2.
+
+    We let n = floor((cap - 1)/x), since it is the largest number of multiples of x below cap.
+
+    In the particular cases of 3 and 5, we can first calculate the sum of multiples of 3 and 5. Note that in this
+    case we are DOUBLE COUNTING the multiples of 3 AND 5. Thus, we have to subtract the sum of multiples of 15.
+
+    Thus SumMultiples(3 or 15) = SumMultiples(3) + SumMultiples(5) - SumMultiples(15)
+"""
+
+N = 1000
+
+def sum_multiples(cap, x):
+    n = int((cap - 1)/x)
+    return int(x * n * (n + 1) / 2)
+
+def ans(n1, n2):
+    return sum_multiples(N, n1) + sum_multiples(N, n2) - sum_multiples(N, n1 * n2)
+
+print(ans(3, 5))
--- 2_even_fibonacci_numbers.py
+++ 2_even_fibonacci_numbers.py
@@ -2,25 +2,81 @@
 # (https://projecteuler.net/problem=2)
 
 
+# cap = 4000000
+
+# def fib():
+#     fn_2 = 1
+#     fn_1 = 2
+#     yield 1
+#     yield 2
+#     while 1:
+#         fn = fn_2 + fn_1
+#         fn_2 = fn_1
+#         fn_1 = fn
+#         yield fn
+
+
+# acc = 0
+# for num in fib():
+#     if num > cap:
+#         break
+#     if num % 2 == 0:
+#         acc += num
+
+# print(acc)
+
+
+
+"""
+    Another observation we could make is that every 3rd number in the fibonacci sequence is even.
+    Let us prove that f(n) is even when n % 3 == 0 and odd otherwise.
+
+    Base cases:
+
+    f(1) = 1
+    f(2) = 1
+    f(3) = 1 + 1 = 2
+
+    Thus, we have proven our proposition true for n = 1 to 3.
+
+    Inductive Assumption: Let us assume that f(k) follows our proposition for n <= k.
+    Prove f(k + 1), f(k + 2), f(k + 3) follows our proposition.
+
+    Proof:
+
+    Case 1:
+    Let us assume the case when k % 3 == 0.
+    Then, according to our proposition f(k) % 2 == 0 and f(k - 1) % 2 == 1.
+
+    By the linearity of mod, we know that
+    f(k + 1) mod 2 = f(k) mod 2 + f(k - 1) mod 2.
+    f(k + 1) mod 2 = (0 + 1) mod 2 = 1 and so f(k + 1) is an odd number.
+
+    Using the same logic:
+
+    f(k + 2) mod 2 = f(k + 1) mod 2 + f(k) mod 2 = (1 + 0) mod 2 = 1
+    f(k + 3) mod 2 = f(k + 2) mod 2 + f(k + 1) mod 2 =  (1 + 1) mod 2 = 0
+
+    Case 2:
+    The inductive hypothesis holds by the same logic as the previous case.
+
+
+    Thus, since f(k + 1), f(k + 2), f(k + 3) hold given that f(k) is true and the base cases are true, we have
+    proven that our proposition is true.
+"""
+
 cap = 4000000
 
-def fib():
-    fn_2 = 1
-    fn_1 = 2
-    yield 1
-    yield 2
-    while 1:
-        fn = fn_2 + fn_1
-        fn_2 = fn_1
-        fn_1 = fn
-        yield fn
+a = 1
+b = 1
+c = 2
 
+s = 0
 
-acc = 0
-for num in fib():
-    if num > cap:
-        break
-    if num % 2 == 0:
-        acc += num
+while c < cap:
+    s += c
+    a = b + c
+    b = a + c
+    c = a + b
 
-print(acc)
+print(s)
